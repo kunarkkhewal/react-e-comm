38de804cc1f8537f2cf50ba9f5f92c2cea376c93
Kunark Khewal <kunark.khewal@yocket.in>
2023-10-30 23:42:54 +0530

    updated app component for interface and code shifted to there files

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import { ProductData } from './Interfaces/interfaces';
 
-import Product from './Components/Product';
-import Pagination from './Components/Pagination';
-
-interface ProductData {
-  id: number,
-  title: string,
-  description: string,
-  price: number,
-  discountPercentage: number,
-  rating: number,
-  stock: number,
-  brand: string,
-  category: string,
-  thumbnail: string,
-  images: string[],
-}
+import HomePageListing from './Components/HomePageListing';
+import Footer from './Components/Footer';
 
 function App() {
   const limit = 5;
@@ -25,16 +12,15 @@ function App() {
   const [products, setProducts] = useState<ProductData[]>([]);
   const [skip, setSkip] = useState<number>(0);
   const [totalPages, setTotalPages] = useState<number>(0);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
   useEffect(() => {
     async function getProducts() {
       const url = `https://dummyjson.com/products?skip=${skip}&limit=${limit}`;
       try {
         const result = await axios.get(url);
-        console.log(' ------- result => ', result);
         setProducts(result.data.products);
-        setTotalPages(result.data.total / limit);
+        setTotalPages(Math.ceil(result.data.total / limit));
       } catch (error) {
         console.error('Error fetching products:', error);
       }
@@ -43,39 +29,32 @@ function App() {
   }, [skip]);
 
   const handlePageChange = (page: number) => {
-    if (page > 0) {
-      setSkip(limit * (page - 1))
+    if (page > 0 && page <= totalPages) {
+      setSkip(limit * (page - 1));
       setCurrentPage(page);
     }
   }
 
-  const productList = products.map((product) => (
-    <Product key={product.id} product={product} />
-  ));
-
   return (
     <div className="flex flex-col justify-between h-screen text-center">
-      <header className="text-xl text-slate-700 bg-red-400">This is header</header>
-      <main className="bg-slate-500 h-full p-4 grid grid-cols-3 gap-4">
-        {productList}
-      </main>
-      <div className="bg-yellow-300 flex justify-between p-4">
-        {currentPage > 1 && (
-          <div className='border border-black p-2' onClick={() => { handlePageChange(currentPage - 1) }}>
-            Prev
-          </div>
-        )}
-        <Pagination
-          currentPage={currentPage}
-          totalPages={totalPages}
-          onPageChange={handlePageChange}
-        />
-        {currentPage < totalPages && (
-          <div className='border border-black p-2' onClick={() => { handlePageChange(currentPage + 1) }}>
-            Next
-          </div>
-        )}
-      </div>
+      <header className="text-xl text-white bg-sky-900 p-4 sm:px-12 flex justify-between">
+        <div>
+          Your Store
+        </div>
+        <div>
+          Cart
+        </div>
+      </header>
+      
+      <HomePageListing 
+        productList={products} 
+      />
+      
+      <Footer 
+        currentPage={currentPage} 
+        totalPages={totalPages}
+        handlePageChange={handlePageChange}
+      />
     </div>
   );
 }
