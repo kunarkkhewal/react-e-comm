9c07a1d934c23a16e0709e0f46af76657ac93bb2
Kunark Khewal <kunark.khewal@yocket.in>
2023-10-31 22:19:06 +0530

    renamed lisiting page

--- src/Components/Listing.tsx
+++ src/Components/Listing.tsx
@@ -2,14 +2,15 @@ import Product from './Product';
 import { ProductData } from '../Interfaces/interfaces';
 
 interface ProductProps {
-    productList: ProductData[]
+    productList: ProductData[],
+    source: string
 }
 
-function HomePageListing({ productList }: ProductProps) {
+function HomePageListing({ productList, source }: ProductProps) {
     return (
         <div className="bg-slate-200 h-full p-4 sm:px-12 grid grid-cols-3 gap-4 overflow-scroll">
         {productList.map((product) => (
-            <Product key={product.id} product={product} />
+            <Product key={product.id} product={product} source={source} />
         ))}
         </div>
     );
