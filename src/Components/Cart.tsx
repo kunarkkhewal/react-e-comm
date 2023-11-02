c96766d688968766bf240581039f3841a4e173dd
Kunark Khewal <kunark.khewal@yocket.in>
2023-11-02 22:26:31 +0530

    updated cart for cart to recieve from context

--- src/Components/Cart.tsx
+++ src/Components/Cart.tsx
@@ -1,21 +1,42 @@
-
-import { TotalCartItemAndSetter } from '../Interfaces/interfaces';
-
+import { useContext } from 'react'
 import Header from './Header';
 import Listing from './Listing';
+// import Footer from './Footer';
 
-function Cart({totalCartItem, setTotalCartItem}: TotalCartItemAndSetter) {
+import { CartContext } from '../App';
+
+function Cart() {
+    const context = useContext(CartContext);
+    if (!context) {
+        return null;
+    }
+    const { cart } = context;
+
+    // const [skip, setSkip] = useState<number>(0);
+    // const [totalPages, setTotalPages] = useState<number>(0);
+    // const [currentPage, setCurrentPage] = useState<number>(1);
+
     return (
         <div className="flex flex-col justify-between h-screen text-center">
-            <Header 
-                totalCartItem={totalCartItem} 
-                setTotalCartItem={setTotalCartItem}
-            />
-            
-            <Listing 
-                productList={[]}  // to be updated
-                source={'CART'}
-            />
+            <Header />
+            {
+                cart.length > 0 && 
+                <Listing 
+                    productList={cart} 
+                    source={'CART'}
+                />
+            }
+            {
+                cart.length === 0 && 
+                (
+                    <div>CART EMPTY</div>
+                )
+            }
+            {/* <Footer 
+                currentPage={currentPage} 
+                totalPages={totalPages}
+                handlePageChange={handlePageChange}
+            /> */}
         </div>
     );
 }
