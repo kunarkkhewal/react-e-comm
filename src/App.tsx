adfc6a16bb3dc065f47f8bf048a5e54f6402a36d
Kunark Khewal <kunark.khewal@yocket.in>
2023-11-02 22:25:51 +0530

    added context api for cart anf total cart items and setter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
-import React, { useState } from 'react';
+import React, { createContext, useState, ReactNode } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ProductData } from './Interfaces/interfaces';
 import Home from './Components/Home';
 import Cart from './Components/Cart';
 
-function App() {
+export const CartContext = createContext<CartContextType | undefined>(undefined);
+interface CartContextType {
+  cart: ProductData[];
+  setCart: (cart: ProductData[]) => void;
+  totalCartItem: number;
+  setTotalCartItem: (totalCartItem: number) => void;
+}
+
+function CartProvider({ children }: { children: ReactNode }) {
   const [cart, setCart] = useState<ProductData[]>([]);
-  const [totalCartItem, setTotalCartItem] = useState<number>(0); // need to update this for cart product
+  const [totalCartItem, setTotalCartItem] = useState<number>(0);
+
+  return (
+    <CartContext.Provider value={{ cart, setCart, totalCartItem, setTotalCartItem }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+// Step 3: Wrap Your App with the Provider
+function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home cart={cart} setCart={setCart}totalCartItem={totalCartItem} setTotalCartItem={setTotalCartItem} />} />
-        <Route path="/cart" element={<Cart cart={cart} setCart={setCart} totalCartItem={totalCartItem} setTotalCartItem={setTotalCartItem} />} />
-      </Routes>
+      <CartProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </CartProvider>
     </Router>
   );
 }
