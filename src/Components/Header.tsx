c4249011170e91e8a26d72a6fe477987bd278e99
Kunark Khewal <kunark.khewal@yocket.in>
2023-11-02 22:26:55 +0530

    updated header for totalcartitem to recieve from context

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,11 +1,18 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom';
-import { TotalCartItemAndSetter } from '../Interfaces/interfaces';
 
-function Header({ totalCartItem }: TotalCartItemAndSetter) {
+import { CartContext } from '../App';
+
+function Header() {
+    const context = useContext(CartContext);
+    if (!context) {
+        return null;
+    }
+    const { totalCartItem } = context;
     return (
         <header className="text-xl text-white bg-sky-900 p-4 sm:px-12 flex justify-between">
             <Link to="/">Your Store</Link>
-            <Link to="/cart">Cart {totalCartItem}</Link>
+            <Link to="/cart">Cart ({totalCartItem})</Link>
         </header>
     );
 }
